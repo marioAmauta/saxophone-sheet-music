dfcdfb11018609a8029750796177575b083b5dbf
marioAmauta <mariodevcl@gmail.com>
2025-04-28 12:39:23 -0400

    feat(session): add cookie handling for user session retrieval

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,10 +1,21 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { cache } from "react";
 
 import { auth } from "@/lib/auth";
+import { authCookieName } from "@/lib/constants";
 import { Roles } from "@/lib/enums";
 
 export const getUserSession = cache(async () => {
+  const authCookie = (await cookies()).get(`${authCookieName}.session_token`);
+
+  if (!authCookie) {
+    return {
+      user: null,
+      session: null,
+      isAdmin: false
+    };
+  }
+
   let session: Awaited<ReturnType<typeof auth.api.getSession>> = null;
 
   try {
