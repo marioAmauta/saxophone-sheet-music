52d052aaf93b000d001c7d603132c48b7b8ec9ff
marioAmauta <mariodevcl@gmail.com>
2025-09-02 00:11:34 -0400

    feat: add album image for song card

--- src/components/song-card.tsx
+++ src/components/song-card.tsx
@@ -1,25 +1,48 @@
 import { Link } from "@/i18n/navigation";
 
+import { ApiRoutes } from "@/lib/api-routes";
 import { SongCardDataType } from "@/lib/types";
 import { capitalize } from "@/lib/utils";
 
 import { Badge } from "@/components/ui/badge";
 
+import { Avatar, AvatarImage } from "./ui/avatar";
+
 type SongCardProps = Omit<SongCardDataType, "slug"> & {
   href: string;
 };
 
-export function SongCard({ href, title, artist: { artistName, musicalGenre } }: SongCardProps) {
+export async function SongCard({ href, title, artist: { artistName, musicalGenre } }: SongCardProps) {
+  const songAlbumImage = await fetch(await ApiRoutes.spotifyApiSong({ songName: title, artistName }), {
+    cache: "force-cache",
+    next: {
+      revalidate: 3600 * 12
+    }
+  });
+
+  const artistImageData = await songAlbumImage.json();
+
+  const smallImage = artistImageData.images[2];
+
   return (
     <div className="bg-secondary hover:bg-primary-foreground relative rounded-lg px-5 py-3 shadow-sm transition-colors">
-      <div className="flex items-center justify-between">
-        <h3 className="break-all">{artistName}</h3>
-        {musicalGenre ? <Badge>{capitalize(musicalGenre)}</Badge> : null}
+      <div className="flex justify-between gap-4">
+        <div className="flex flex-col gap-2">
+          <Link href={href} className="text-3xl font-medium">
+            <span className="absolute inset-0" />
+            {title}
+          </Link>
+          {musicalGenre ? <Badge>{capitalize(musicalGenre)}</Badge> : null}
+        </div>
+        <div className="grid">
+          {smallImage ? (
+            <Avatar className="ml-auto size-20">
+              <AvatarImage src={smallImage.url} />
+            </Avatar>
+          ) : null}
+          <h3 className="break-all">{artistName}</h3>
+        </div>
       </div>
-      <Link href={href} className="text-3xl font-medium">
-        <span className="absolute inset-0" />
-        {title}
-      </Link>
     </div>
   );
 }
